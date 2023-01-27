16c5de402c0a57c2837ff05a064f6bd3ab146304
Tueur-dombres <sabat.esteban@gmail.com>
2023-01-27 11:45:12 +0100

    début

---
+++ bubbles.py
@@ -0,0 +1,39 @@
+import pygame
+from pygame.locals import *
+from random import *
+
+size = w, h = (1280,720)
+screen = pygame.display.set_mode(size)
+horloge = pygame.time.Clock()
+
+col = {"BLACK":(0,0,0), "WHITE":(255,255,255), "BLUE":(0,0,255), "RED":(255,0,0), "GREEN":(0,255,0)}
+bubbles_colour = [e for i,e in col.items() if i not in ["WHITE"]]
+
+bubbles = []
+spawn_bubbles = 0
+
+continuer = True    
+while continuer:
+    horloge.tick(240)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = False
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                continuer = False
+    screen.fill(col["WHITE"])
+    spawn_bubbles += 1
+    suppr = []
+    for i in range(len(bubbles)):
+        bubble = bubbles[i]
+        bubble["radius"] += .05
+    bubbles = [bubble for bubble in bubbles if bubble["radius"] <= 100]
+
+
+    if spawn_bubbles >= 240:
+        bubbles += [{"x" : randint(100, w-100), "y" : randint(100, h-100), "radius" : 10, "colour" : choice(bubbles_colour)}]
+        spawn_bubbles = 0
+    
+    for bubble in bubbles :
+        pygame.draw.circle(screen, bubble["colour"], (bubble["x"], bubble["y"]), bubble["radius"])
+    pygame.display.flip()
