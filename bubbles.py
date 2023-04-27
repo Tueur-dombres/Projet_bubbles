2251d78723e46a06b0ab49bf625f30d11e0204b5
Tueur-dombres <sabat.esteban@gmail.com>
2023-04-27 10:01:13 +0200

    quelques commentaires

--- bubbles.py
+++ bubbles.py
@@ -5,18 +5,33 @@ from math import sqrt, ceil
 
 pg.init()
 
+#initialisation de la fenêtre
 size_screen = w, h = (1280,720)
 w_gameover, h_gameover = 600,300
 screen = pg.display.set_mode(size_screen)
 horloge = pg.time.Clock()
+record = 0
+
+#listing des couleurs
 col = {"BLACK":(0,0,0), "BLUE":(0,0,255), "RED":(255,0,0), "GREEN":(0,255,0), "MAGENTA":(255,0,255), "JAUNE":(255,255,0), "CYAN":(0,255,255), "WHITE":(255,255,255)}
 couleurs = [*col.values()][:-1]
+
+#préparation des polices d'écritures
 font_very_big = pg.font.SysFont("arial", 100)
 font_big = pg.font.SysFont("arial", 48)
 font = pg.font.SysFont("arial", 36)
 font_petit = pg.font.SysFont("arial", 24)
-record = 0
 
+#initialisation tailles et positions des boutons
+button_restart_gameover = Rect(w/2-160, h/2+60, 128, 36)
+button_menu_gameover = Rect(w/2+52, h/2+60, 90, 36)
+button_start_menu = Rect(w/2-344/2, h/2-50, 344, 112)
+button_credits_menu = Rect(w/2-370, h/2+200, 230, 60)
+button_regles_menu = Rect(w/2+149, h/2+200, 212, 60)
+button_retour_menu = Rect(w/2-143, h-100, 286, 48)
+button_quitter_menu = Rect(40, 40, 180, 48)
+
+#fonction à appeler pour remettre les valeurs à 0
 def start():
     global bubbles, spawn_bubbles, score, vies, tempo_bubbles, temps_restant, speed_modif
     bubbles = []
@@ -28,13 +43,7 @@ def start():
     speed_modif = 1
 start()
 
-button_restart_gameover = Rect(w/2-160, h/2+60, 128, 36)
-button_menu_gameover = Rect(w/2+52, h/2+60, 90, 36)
-button_start_menu = Rect(w/2-344/2, h/2-50, 344, 112)
-button_credits_menu = Rect(w/2-370, h/2+200, 230, 60)
-button_regles_menu = Rect(w/2+149, h/2+200, 212, 60)
-button_retour_menu = Rect(w/2-143, h-100, 286, 48)
-button_quitter_menu = Rect(40, 40, 180, 48)
+#création d'une classe Circle, qui fonctionne comme la classe Rect native de pygame 
 class Circle:
     def __init__(self, x, y, radius, colour):
         self.x = x
@@ -42,82 +51,85 @@ class Circle:
         self.radius = radius
         self.colour = colour
     
+    #vérifier si un point appartient au cercle, éventuellement si un point est à une certaine distance avec le paramètre optionnel size
     def collidepoint(self, x, y, size = 0):
         if sqrt((self.x-x)**2+(self.y-y)**2) < (size if size else self.radius):
             return True
         else:
             return False
-        
-gamemode = "menu"
-continuer = True    
+
+gamemode = "menu" #on commence au menu principal
+continuer = True #initalisation de la boucle de jeu
 while continuer:
     horloge.tick(240)
     for event in pg.event.get():
-        if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+        if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE: #pour quitter le jeu avec la touche échap, ou la croix
             continuer = False
-        elif event.type == MOUSEBUTTONDOWN and event.button == 1:
-            x, y = event.pos
-            if gamemode == "play":
+        elif event.type == MOUSEBUTTONDOWN and event.button == 1: #click gauche
+            x, y = event.pos #récupération de la position de la souris
+            if gamemode == "play": #si le jeu es lancé
                 suppr = []
-                for i in range(len(bubbles)):
+                for i in range(len(bubbles)): #on parcours la liste des bubbles
                     bubble = bubbles[i]
-                    if bubble.collidepoint(x,y):
-                        if bubble.colour == col["BLACK"]:
-                            vies -= 1
-                            suppr += [i]
-                        else:
-                            bubble.colour = choice(couleurs)
-                tempo_bubbles += [[bubbles[i],0] for i in range(len(bubbles)) if i in suppr]
-                bubbles = [bubbles[i] for i in range(len(bubbles)) if i not in suppr]
-            elif gamemode == "gameover":
-                if button_restart_gameover.collidepoint(x,y):
+                    if bubble.collidepoint(x,y): #si la bubble est cliquée
+                        if bubble.colour == col["BLACK"]: #pour les noires
+                            vies -= 1 #perte d'une vie
+                            suppr += [i] #ajout à la liste des futures disparition
+                        else: #pour les autres couleurs
+                            bubble.colour = choice(couleurs) #changement de couleur 
+                tempo_bubbles += [[bubbles[i],0] for i in range(len(bubbles)) if i in suppr] #le temps que la bulle fasse son animation de disparition
+                bubbles = [bubbles[i] for i in range(len(bubbles)) if i not in suppr] #suppression des bubbles à supprimer dans la liste principale
+            elif gamemode == "gameover": #s'il on est sur le menu gameover
+                if button_restart_gameover.collidepoint(x,y): #relancer le jeu
                     gamemode = "play"
                     start()
-                elif button_menu_gameover.collidepoint(x,y):
+                elif button_menu_gameover.collidepoint(x,y): #retour au menu principal
                     gamemode = "menu"
-            elif gamemode == "menu":
-                if button_start_menu.collidepoint(x,y):
+            elif gamemode == "menu": #s'il on est au menu principal
+                if button_start_menu.collidepoint(x,y): #lancer le jeu
                     gamemode = "play"
                     start()
-                elif button_credits_menu.collidepoint(x,y):
+                elif button_credits_menu.collidepoint(x,y): #aller dans les crédits
                     gamemode = "credits"
-                elif button_regles_menu.collidepoint(x,y):
+                elif button_regles_menu.collidepoint(x,y): #aller dans les règles
                     gamemode = "regles"
-                elif button_quitter_menu.collidepoint(x,y):
+                elif button_quitter_menu.collidepoint(x,y): #quitter le jeu avec le bouton quitter
                     continuer = False
-            elif gamemode in ["credits","regles"]:
-                if button_retour_menu.collidepoint(x,y):
+            elif gamemode in ["credits","regles"]: #s'il on est dans les crédits ou les règles
+                if button_retour_menu.collidepoint(x,y): #retour au menu principal
                     gamemode = "menu"
 
 
 
-    screen.fill(col["WHITE"])
+    screen.fill(col["WHITE"]) #fond blanc
 
-    if gamemode == "play":
+    if gamemode == "play": #si le jeu est lancé
+        #gestion des timers pour l'apparition, le temp restant, et la vitesse de jeu
         spawn_bubbles += 1
         temps_restant -= 1
-        speed_modif *= 1.00005
-        #aggrandissement et comptage des bubbles
-        colours_count = {i:[] for i in couleurs[1:]}
+        speed_modif *= 1.00005 #la modification de vitesse d'apparition et de croissance varie de manière exponentielle entre 1 et 2 en 60sec
+
+        #aggrandissement des bubbles et comptage des bubbles avec la création d'un dictionnaire qui classe les bubbles par couleur
+        colours_count = {i:[] for i in couleurs[1:]} 
         for i in range(len(bubbles)):
             bubble = bubbles[i]
             bubble.radius += .05*speed_modif
             if bubble.colour != col["BLACK"]:
                 colours_count[bubble.colour] += [i]
 
-        #suppression des triplets
+        #recherche des triplets de couleurs
         suppr = []
         for i,e in colours_count.items():
             if len(e) >= 3:
                 suppr += e
                 score += 1
 
-        tempo_bubbles += [[bubbles[i],0] for i in range(len(bubbles)) if i in suppr]
-        bubbles = [bubbles[i] for i in range(len(bubbles)) if i not in suppr]
-        bubbles = [bubble for bubble in bubbles if bubble.radius <= 100]
+        tempo_bubbles += [[bubbles[i],0] for i in range(len(bubbles)) if i in suppr] #le temps que la bulle fasse son animation de disparition
+        bubbles = [bubbles[i] for i in range(len(bubbles)) if i not in suppr] #suppression des triplets
+        bubbles = [bubble for bubble in bubbles if bubble.radius <= 100] #disparition des bubbles trop grande
 
         #génération
-        if spawn_bubbles >= 240/speed_modif:
+        if spawn_bubbles >= 240/speed_modif: #quand le compteur est assez grand, en fonction de la vitesse
             #choisir une position pas trop proche d'une autre bubble pour qu'elle ne puisse pas se toucher
             x,y = randint(100, w-100), randint(100, h-100) #une première positon aléatoire
             while sum(1 for bubble in bubbles if bubble.collidepoint(x,y,size=200))!=0: #s'il la position est trop proche d'une bubble
