982f2439a1c710d523f669667e6e1a925162fbfd
Tueur-dombres <sabat.esteban@gmail.com>
2023-04-27 10:33:05 +0200

    plus de commentaires

--- bubbles.py
+++ bubbles.py
@@ -79,13 +79,13 @@ while continuer:
                             bubble.colour = choice(couleurs) #changement de couleur 
                 tempo_bubbles += [[bubbles[i],0] for i in range(len(bubbles)) if i in suppr] #le temps que la bulle fasse son animation de disparition
                 bubbles = [bubbles[i] for i in range(len(bubbles)) if i not in suppr] #suppression des bubbles à supprimer dans la liste principale
-            elif gamemode == "gameover": #s'il on est sur le menu gameover
+            elif gamemode == "gameover": #dans le menu gameover
                 if button_restart_gameover.collidepoint(x,y): #relancer le jeu
                     gamemode = "play"
                     start()
                 elif button_menu_gameover.collidepoint(x,y): #retour au menu principal
                     gamemode = "menu"
-            elif gamemode == "menu": #s'il on est au menu principal
+            elif gamemode == "menu": #dans le menu principal
                 if button_start_menu.collidepoint(x,y): #lancer le jeu
                     gamemode = "play"
                     start()
@@ -95,7 +95,7 @@ while continuer:
                     gamemode = "regles"
                 elif button_quitter_menu.collidepoint(x,y): #quitter le jeu avec le bouton quitter
                     continuer = False
-            elif gamemode in ["credits","regles"]: #s'il on est dans les crédits ou les règles
+            elif gamemode in ["credits","regles"]: #dans les crédits ou les règles
                 if button_retour_menu.collidepoint(x,y): #retour au menu principal
                     gamemode = "menu"
 
@@ -138,38 +138,53 @@ while continuer:
             bubbles += [Circle(x,y, 10, c)] #ajout de la nouvelle bubble à la liste
             spawn_bubbles = 0 #reset du timer
     
-    if gamemode in ["play","gameover","transition_gameover"]:   
+    if gamemode in ["play","gameover","transition_gameover"]: #pour les modes dans lesquels on doit afficher les bubbles
+
+        #dessin des animations de disparition
         suppr = []
         for i in range(len(tempo_bubbles)):
             bubble,size = tempo_bubbles[i][0],tempo_bubbles[i][1]
-            pg.draw.circle(screen, bubble.colour, (bubble.x, bubble.y), bubble.radius)
+            pg.draw.circle(screen, bubble.colour, (bubble.x, bubble.y), bubble.radius) #dessin de la bubble originale
+
+            #on dessine l'intérieur de la bubble en blanc
             if bubble.colour == col["BLACK"]:
-                pg.draw.polygon(screen, col["WHITE"], [(bubble.x,bubble.y-size),(bubble.x+size,bubble.y),(bubble.x,bubble.y+size),(bubble.x-size,bubble.y)])
+                pg.draw.polygon(screen, col["WHITE"], [(bubble.x,bubble.y-size),(bubble.x+size,bubble.y),(bubble.x,bubble.y+size),(bubble.x-size,bubble.y)]) #un losange pour les noires
             else:
-                pg.draw.circle(screen, col["WHITE"], (bubble.x, bubble.y), size)
-            tempo_bubbles[i][1]+=.5*speed_modif
-            if tempo_bubbles[i][1] >= bubble.radius:
+                pg.draw.circle(screen, col["WHITE"], (bubble.x, bubble.y), size) #un cercle pour les autres couleurs
+
+            tempo_bubbles[i][1]+=.5*speed_modif #agrandissement des formes intérieur en focntion de la vitesse
+            if tempo_bubbles[i][1] >= bubble.radius: #recherche des bubbles dont la taille de la forme intérieure atteint la taille de la bubble originale
                 suppr += [i]
-        tempo_bubbles = [tempo_bubbles[i] for i in range(len(tempo_bubbles)) if i not in suppr]
+        tempo_bubbles = [tempo_bubbles[i] for i in range(len(tempo_bubbles)) if i not in suppr] #suppression des bubbles à supprimer
 
         for bubble in bubbles :
-            pg.draw.circle(screen, bubble.colour, (bubble.x, bubble.y), bubble.radius)
+            pg.draw.circle(screen, bubble.colour, (bubble.x, bubble.y), bubble.radius) #dessin des bubbles classiques
         
+        #affichage du temps restant
         text_temps_restant = font.render("temps restant : "+str(int(ceil(temps_restant/240))), 1, col["BLACK"])
         screen.blit(text_temps_restant, (965, 20))
 
+        #affichage du score
         text_score = font.render("score : "+str(score), 1, col["BLACK"])
         screen.blit(text_score, (20, 20))
 
+        #affichage des vies restantes
         text_vies = font.render("vies restantes : "+str(vies), 1, col["BLACK"])
         screen.blit(text_vies, (980, 60))
 
-    if gamemode == "gameover":
+    if gamemode == "transition_gameover": #durant l'apparition de la fenêtre de gameover
+        compte_transition_gameover += 1 #timer
+        pg.draw.rect(screen, col["BLACK"], Rect(w/2-compte_transition_gameover,h/2-compte_transition_gameover/2,compte_transition_gameover*2,compte_transition_gameover)) #dessin de la fenêtre en fontion avec une taille en fontion du timer
+        if compte_transition_gameover == w_gameover/2: #quand la fenêtre à atteint sa taille
+            gamemode = "gameover" #passage au menu gameover
+
+    elif gamemode == "gameover": #dans le menu gameover
+        #dessin de la fenêtre de gameover avec le titre
         pg.draw.rect(screen, col["BLACK"], Rect(w/2-w_gameover/2, h/2-h_gameover/2, w_gameover, h_gameover))
         text_gameover = font.render("GAMEOVER", 1, col["WHITE"])
         screen.blit(text_gameover, (w/2-105, h/2-h_gameover/2+50))
 
-        #affichage du score
+        #affichage des différentes lignes de calcul du score, centrées
         text_score_base = font_petit.render(f"Score = {score}", 1, col["WHITE"])
         screen.blit(text_score_base, (w/2-60,h/2-35))
         text_score_vies = font_petit.render(f"Vies restantes = {vies}", 1, col["WHITE"])
@@ -177,7 +192,7 @@ while continuer:
         text_score_total = font_petit.render(f"Score total = {score} + 5 x {vies} = {score+5*vies}", 1, col["WHITE"])
         screen.blit(text_score_total, (w/2-150,h/2+15))
         
-        #affichage des boutons
+        #affichage des boutons restart et retour au menu
         text_restart = font_petit.render("RESTART", 1, col["WHITE"])
         screen.blit(text_restart, (button_restart_gameover.x+10, button_restart_gameover.y+5))
         pg.draw.rect(screen, col["WHITE"], button_restart_gameover, width = 4)
@@ -186,7 +201,9 @@ while continuer:
         pg.draw.rect(screen, col["WHITE"], button_menu_gameover, width = 4)
 
     elif gamemode == "menu":
-        screen.fill(col["BLACK"])
+        screen.fill(col["BLACK"]) #fond noir
+
+        #titre
         text_menu = font_big.render("MENU PRINCIPAL", 1, col["WHITE"])
         screen.blit(text_menu, (w/2-205, 150))
 
@@ -194,48 +211,51 @@ while continuer:
         text_record = font.render(f"Record : {record}", 1, col["WHITE"])
         screen.blit(text_record, (1000, 40))
 
-        #affichage des boutton
+        #affichage des boutton start, crédits, règles, et quitter
         text_start = font_very_big.render("START", 1, col["WHITE"])
         screen.blit(text_start, (button_start_menu.x+10, button_start_menu.y+2))
         pg.draw.rect(screen, col["WHITE"], button_start_menu, width = 7)
+
         text_credits = font_big.render("CREDITS", 1, col["WHITE"])
         screen.blit(text_credits, (button_credits_menu.x+10, button_credits_menu.y+5))
         pg.draw.rect(screen, col["WHITE"], button_credits_menu, width = 4)
+
         text_regles = font_big.render("REGLES", 1, col["WHITE"])
         screen.blit(text_regles, (button_regles_menu.x+10, button_regles_menu.y+5))
         pg.draw.rect(screen, col["WHITE"], button_regles_menu, width = 4)
+
         text_quitter = font.render("QUITTER", 1, col["WHITE"])
         screen.blit(text_quitter, (button_quitter_menu.x+10, button_quitter_menu.y+5))
         pg.draw.rect(screen, col["WHITE"], button_quitter_menu, width = 4)
     
-    elif gamemode in ["credits","regles"]:
-        screen.fill(col["BLACK"])
+    elif gamemode in ["credits","regles"]: #pour les crédits et les règles
+        screen.fill(col["BLACK"]) #fond noir
 
         #affichage du bouton retour menu
         text_retour = font.render("RETOUR MENU", 1, col["WHITE"])
         screen.blit(text_retour, (button_retour_menu.x+10,button_retour_menu.y+5))
         pg.draw.rect(screen, col["WHITE"], button_retour_menu, width = 4)
 
-        if gamemode == "credits":
-            #titre
+        if gamemode == "credits": #pour les crédits
+            #affichage du titre
             text_titre_credits = font_big.render("Crédits", 1, col["WHITE"])
             screen.blit(text_titre_credits, (w/2-75, 100))
 
-            #crédits
+            #affichage de texte des crédits
             texts_credits = ["Esteban Sabatier : développeur front-end et back-end",
-                            "Alienor Librecht : vidéaste et Testeuse/QA",
+                            "Alienor Libbrecht : vidéaste et Testeuse/QA",
                             "Charlotte Samama : écriture et management",
-                            "Noelline Knapp-Dubois : scénariste et game designer"]
+                            "Noelline Knapp--Dubois : scénariste et game designer"]
             
             for i in range(len(texts_credits)):
                 screen.blit(font.render(texts_credits[i], 1, col["WHITE"]), (w/2-420, 300 + 45*i))
 
         elif gamemode == "regles":
-            #titre
+            #affichage du titre
             text_titre_regles = font_big.render("Règles", 1, col["WHITE"])
             screen.blit(text_titre_regles, (w/2-80, 100))
 
-            #règles
+            #affichage du texte des règles
             texts_regles = ["Line 1",
                             "Line 2",
                             "Line 3",
@@ -244,15 +264,9 @@ while continuer:
             for i in range(len(texts_regles)):
                 screen.blit(font.render(texts_regles[i], 1, col["WHITE"]), (w/2-420, 300 + 45*i))
 
-    elif gamemode == "transition_gameover":
-        compte_transition_gameover += 1
-        pg.draw.rect(screen, col["BLACK"], Rect(w/2-compte_transition_gameover,h/2-compte_transition_gameover/2,compte_transition_gameover*2,compte_transition_gameover))
-        if compte_transition_gameover == w_gameover/2:
-            gamemode = "gameover"
-
     if (vies <= 0 or temps_restant <= 0) and gamemode == "play" and not tempo_bubbles: #si l'on a plus de vie, ou de temps
-        gamemode = "transition_gameover"
-        compte_transition_gameover = 0
-        record = max(record, score+5*vies)
+        gamemode = "transition_gameover" #on lance la transition
+        compte_transition_gameover = 0 #reset du timer de transition
+        record = max(record, score+5*vies) #actualisation du record
 
     pg.display.flip()
